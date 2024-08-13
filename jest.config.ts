63bdf9d999f09a33419b83301be64d096bebd8ea
Daniel McQuillen <daniel@mcquilleninteractive.com>
2024-08-13 10:36:37 +1000

    Updating to fix jest issues

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,14 @@ export default {
   clearMocks: true,
   testEnvironment: "jsdom",
   testMatch: [
-      "<rootDir>/kinesinlms_components/**/*.test.ts",
-      "<rootDir>/kinesinlms_components/**/*.test.tsx",
+    "<rootDir>/kinesinlms_components/**/*.test.ts",
+    "<rootDir>/kinesinlms_components/**/*.test.tsx",
   ],
   setupFilesAfterEnv: [
-      "<rootDir>/jest-setup.ts",
-  ]
+    "<rootDir>/jest-setup.ts",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "babel-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
 };
-
