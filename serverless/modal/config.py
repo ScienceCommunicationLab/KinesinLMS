3f8fc7a92170a399fa6925639632285509900683
Daniel McQuillen <daniel@mcquilleninteractive.com>
2024-11-11 12:18:44 +1100

    Refactor modal.com methods

---
+++ serverless/modal/config.py
@@ -0,0 +1,24 @@
+JUPYTER_SERVER_CONFIG = """
+c.ServerApp.tornado_settings = {
+    "headers": {
+        "Content-Security-Policy": "default-src 'self' * 'unsafe-inline' 'unsafe-eval' ws: wss: blob: data:; "
+        "frame-ancestors 'self' *;",
+        "X-Frame-Options": "ALLOW-FROM https://kinesinlms-8b588e478a49.herokuapp.com http://localhost:8001"
+    }
+}
+c.ServerApp.root_dir = '/root/workspace'
+    """
+
+
+def config_jupyterlab(notebook_filename: str = None):
+    # Create jupyter_server_config.py with the CSP headers
+    # we need to be able to load the Jupyter Lab in an iFrame.
+    config = JUPYTER_SERVER_CONFIG
+
+    if notebook_filename:
+        config += f"c.LabApp.default_url = '/lab/tree/{notebook_filename}'"
+
+    # Write config to file
+    with open("/root/.jupyter/jupyter_server_config.py", "w") as f:
+        f.write(config)
+    print("  - done saving Jupyter config")
--- serverless/modal/make_jupyter_hub.py
+++ serverless/modal/make_jupyter_hub.py
@@ -3,48 +3,72 @@ import os
 import shutil
 import subprocess
 from pathlib import Path
+from secrets import token_urlsafe
 from typing import Dict, List
 
 import modal
 
-# Define a persistent volume for SQLite
-sqlite_volume = modal.Volume.from_name("sqlite-data-volume", create_if_missing=True)
+from config import config_jupyterlab
 
-app = modal.App("my_jupyter_hub")
-app.image = modal.Image.debian_slim().pip_install(
-    "jupyterlab", "matplotlib", "pandas", "numpy", "sqlalchemy", "scipy"
-)
+# Configuration
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-s3_secret = modal.Secret.from_name(
-    "aws-s3-bucket-secrets",
-    required_keys=["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"],
-)
-
-
-# By convention, we're storing JupyterLab ipynb files as the
-# 'file_resource' File property of a Django 'Resource' model.
+JUPYTER_PORT = 8888
+# By convention, we're storing JupyterLab .ipynb files as the
+# 'file_resource' File property of a KinesinLMS 'Resource' model.
 # These files will be stored in the '/media/block_resources' directory
 # of the 'kinesinlms' bucket on S3. So we'll read them straight from
-# there as we've stored the  S3 credentials in the 'saws-s3-bucket-secrets' variable.
-MOUNT_PATH = Path("/kinesinlms")
-BLOCK_RESOURCES_PATH = MOUNT_PATH / "media" / "block_resources"
-SQLITE_PATH = Path("/sqlite_data")
+# there as we've stored the S3 credentials in the 'aws-s3-bucket-secrets' variable.
+AWS_MOUNT_PATH = Path("/kinesinlms")
+AWS_BLOCK_RESOURCES_PATH = AWS_MOUNT_PATH / "media" / "block_resources"
+RESOURCES_PATH = Path("/resources_data")
+
+# Define a secret to store the AWS S3 credentials
+# The app is going to read in the notebook as well as any
+# accompanying resources from the S3 bucket.
+s3_secret = modal.Secret.from_name(
+    "aws-s3-bucket-secrets",
+    required_keys=[
+        "AWS_ACCESS_KEY_ID",
+        "AWS_SECRET_ACCESS_KEY",
+    ],
+)
+
+# Apps and Volumes
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Define a persistent volume for KinesinLMS resources
+# e.g. SQLite databases, CSV files, etc.
+resources_volume = modal.Volume.from_name(
+    "resources-data-volume",
+    create_if_missing=True,
+)
+
+# This is the modal app that's going to run the Jupyter Lab server.
+app = modal.App("my_jupyter_hub")
+app.image = modal.Image.debian_slim().pip_install(
+    "jupyterlab",
+    "matplotlib",
+    "pandas",
+    "numpy",
+    "sqlalchemy",
+    "scipy",
+)
+
 
 # JUPYTER LAB SERVER
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-# Function that starts a Jupyter server
-# and returns a URL for a student to access it.
 @app.function(
     timeout=1_500,
     volumes={
-        MOUNT_PATH: modal.CloudBucketMount(
+        AWS_MOUNT_PATH: modal.CloudBucketMount(
             bucket_name="kinesinlms",
             read_only=True,
             secret=s3_secret,
         ),
-        SQLITE_PATH: sqlite_volume,
+        RESOURCES_PATH: resources_volume,
     },
 )
 def run_jupyter(
@@ -52,6 +76,7 @@ def run_jupyter(
     notebook_filename=None,
     resources: List[Dict] = [],
     extra_pip_packages=[],
+    access_token=None,
 ):
     """
     Start a Jupyter Lab server and return the URL.
@@ -75,8 +100,11 @@ def run_jupyter(
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
+    if not access_token:
+        access_token = token_urlsafe(16)
+
     if extra_pip_packages:
-        print(f"Installing packages: {extra_pip_packages}")
+        print(f"run_jupyter(): Installing packages: {extra_pip_packages}")
         try:
             subprocess.check_call(
                 [
@@ -96,26 +124,25 @@ def run_jupyter(
 
     print(f"Starting Jupyter Lab. notebook_filename: {notebook_filename}")
 
-    jupyter_port = 8888
-    token = "1234"  # temp
-
     # Create config and workspace directories if they don't exist
     os.makedirs("/root/.jupyter", exist_ok=True)
     workspace_dir = Path("/root/workspace")
     os.makedirs(workspace_dir, exist_ok=True)
 
-    # HANDLE INCOMING NOTEBOOK
-    # Copy notebook file if provided so that it's available in the workspace
-    # immediately upon starting Jupyter Lab.
     local_notebook_path = None
     if notebook_filename:
+        # HANDLE INCOMING NOTEBOOK
+        # Copy notebook file if provided so that it's available in the workspace
+        # immediately upon starting Jupyter Lab.
         try:
-            s3_path = BLOCK_RESOURCES_PATH / notebook_filename
+            s3_path = AWS_BLOCK_RESOURCES_PATH / notebook_filename
             local_notebook_path = workspace_dir / notebook_filename
-            print(f"Copying notebook from {s3_path} to {local_notebook_path}")
+            print(
+                f"run_jupyter(): Copying notebook from {s3_path} to {local_notebook_path}"
+            )
             shutil.copy2(s3_path, local_notebook_path)
             print(
-                f"  - successfully copied notebook from {s3_path} to "
+                f"run_jupyter()  - successfully copied notebook from {s3_path} to "
                 f"{local_notebook_path}"
             )
 
@@ -123,12 +150,14 @@ def run_jupyter(
             logger.error(f"Error copying notebook file: {e}")
             raise
 
-    # HANDLE INCOMING RESOURCES
     if resources:
-        print("Processing resources...")
+        # HANDLE INCOMING KINESINLMS RESOURCES
+        # Store them in the attached volume so they're available to the notebook
+        print("run_jupyter(): Processing resources...")
+        os.makedirs(RESOURCES_PATH, exist_ok=True)
         for resource in resources:
             print(
-                f" - resource type: {resource.get('type')} "
+                f"run_jupyter():  - resource type: {resource.get('type')} "
                 f"filename: {resource.get('filename')}"
             )
             try:
@@ -137,11 +166,12 @@ def run_jupyter(
 
                 if not filename or not resource_type:
                     logger.warning(
-                        f"Skipping resource with missing filename or type: {resource}"
+                        f"run_jupyter(): Skipping resource with missing "
+                        f"filename or type: {resource}"
                     )
                     continue
 
-                s3_path = BLOCK_RESOURCES_PATH / filename
+                s3_path = AWS_BLOCK_RESOURCES_PATH / filename
 
                 # For SQLite files, copy to workspace to match notebook's working dir
                 if resource_type in ["SQLITE", "CSV"]:
@@ -149,60 +179,42 @@ def run_jupyter(
                     base_filename = Path(filename).name
                     dest_path = workspace_dir / base_filename
                     print(
-                        f"  - copying {resource_type} file from {s3_path} "
+                        f"  - copying {resource_type} file "
+                        f"from {s3_path} "
                         f"to {dest_path}"
                     )
                     shutil.copy2(s3_path, dest_path)
                     os.chmod(dest_path, 0o666)  # Ensure file permissions allow rw
-                    print(f"  - successfully copied SQLite file: {base_filename}")
+                    print(
+                        f"run_jupyter():   - successfully copied "
+                        f"SQLite file: {base_filename}"
+                    )
                 else:
-                    print(f"Unsupported resource type: {resource_type}")
+                    print(f"run_jupyter(): Unsupported resource type: {resource_type}")
 
             except Exception as e:
-                logger.error(f"Error copying resource {filename}: {e}")
+                logger.error(f"run_jupyter(): Error copying resource {filename}: {e}")
                 raise
     else:
-        print("### No resources!")
+        print("run_jupyter(): No resources!")
 
-    # Create jupyter_server_config.py with the CSP headers
-    # we need to be able to load the Jupyter Lab in an iFrame.
-    print("Saving Jupyter config...")
-    config = """
-c.ServerApp.tornado_settings = {
-    "headers": {
-        "Content-Security-Policy": "default-src 'self' * 'unsafe-inline' 'unsafe-eval' ws: wss: blob: data:; "
-        "frame-ancestors 'self' *;",
-        "X-Frame-Options": "ALLOW-FROM https://kinesinlms-8b588e478a49.herokuapp.com http://localhost:8001"
-    }
-}
-c.ServerApp.root_dir = '/root/workspace'
-    """
+    for resource in resources:
+        resource_type = resource.get("type")
+        if resource_type == "SQLITE":
+            # Even thought we already saved the sqlite file to the workspace,
+            # we still need to store the actual database file that sqlite
+            # creates when it opens the database. We'll store it in the
+            # resources directory so that it's available to the notebook.
+            os.environ["SQLITE_DATABASE_PATH"] = str(RESOURCES_PATH / "database.db")
 
-    # style-src 'self' https://kinesinlms-8b588e478a49.herokuapp.com; script-src 'self' https://kinesinlms-8b588e478a49.herokuapp.com; connect-src 'self' wss://*;
-
-    # Add notebook-specific configuration if a notebook is provided
-    if notebook_filename:
-        config += f"""
-c.LabApp.default_url = '/lab/tree/{notebook_filename}'
-"""
-
-    # Write config to file
-    with open("/root/.jupyter/jupyter_server_config.py", "w") as f:
-        f.write(config)
-    print("  - done saving Jupyter config")
-
-    # Create SQLite directory if it doesn't exist
-    os.makedirs(SQLITE_PATH, exist_ok=True)
-
-    # Set environment variable for SQLite database path
-    os.environ["SQLITE_DATABASE_PATH"] = str(SQLITE_PATH / "database.db")
+    config_jupyterlab(notebook_filename=notebook_filename)
 
     print("Starting Jupyter Lab as tunnel...")
-    with modal.forward(jupyter_port) as tunnel:
-        url = tunnel.url + "/?token=" + token
+    with modal.forward(JUPYTER_PORT) as tunnel:
+        url = tunnel.url + "/?token=" + access_token
 
         q.put(url, block=False)
-        print(f"Starting Jupyter at {url}")
+        print(f"run_jupyter(): Starting Jupyter at {url}")
 
         subprocess.run(
             [
@@ -215,7 +227,7 @@ c.LabApp.default_url = '/lab/tree/{notebook_filename}'
                 "--LabApp.allow_origin='*'",
                 "--LabApp.allow_remote_access=1",
             ],
-            env={**os.environ, "JUPYTER_TOKEN": token, "SHELL": "/bin/bash"},
+            env={**os.environ, "JUPYTER_TOKEN": access_token, "SHELL": "/bin/bash"},
         )
 
 
@@ -226,6 +238,7 @@ def spawn_jupyter(
     notebook_filename=None,
     extra_pip_packages=[],
     resources=[],
+    access_token=None,
 ):
     """
     Spawn a Jupyter Lab server and return the URL.
@@ -236,6 +249,7 @@ def spawn_jupyter(
         extra_pip_packages: list        A list of extra pip packages to install.
         resources: list                 A list of KinesinLMS 'resources' that
                                         accompany the notebook (e.g. a SQLite file).
+        access_token: str               A secret token to authenticate the user.
 
     Raises:
         Exception
@@ -255,13 +269,16 @@ def spawn_jupyter(
             )
             if extra_pip_packages:
                 print(f"  -  Extra pip packages: {extra_pip_packages}")
+
+            print("spawn_jupyter(): spawinging Jupyter Lab...")
             run_jupyter.spawn(
                 q,
                 notebook_filename=notebook_filename,
                 extra_pip_packages=extra_pip_packages,
                 resources=resources,
+                access_token=access_token,
             )
-            print("spawn_jupyter(): Getting url...")
+            print("spawn_jupyter(): Getting Jupyter Lab url...")
             url = q.get()
             print(f"spawn_jupyter(): Jupyter is ready at {url}")
             return url
